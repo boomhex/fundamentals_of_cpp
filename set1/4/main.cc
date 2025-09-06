8bdb09a107958e8eb7b0296c71b1decccc6e4f92
boomhex <98780985+boomhex@users.noreply.github.com>
2025-09-06 17:29:12 +1000

    finished ex4

---
+++ set1/4/main.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+namespace {                 			// anonymous name space
+
+char const rawStringLiteral[] =         // define the Raw String Literal
+R"delimiter(^\s+Encryption key:(\w+)
+^\s+Quality=(\d+)
+^\s+E?SSID:"([[:print:]]+)"
+^\s+ssid="([[:print:]]+)"
+)delimiter";
+
+}   // namespace ends
+
+int main(int argc, char* argv[])
+{
+
+    std::cout << rawStringLiteral;		 // print raw string literal to stream.
+
+}
