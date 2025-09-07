cbe90c0a5162dea96847bbb040da03b56687e111
RubenKorbeeck <rubenkorbeeck@gmail.com>
2025-09-07 10:41:08 +1000

    comments

--- set1/8/main.cc
+++ set1/8/main.cc
@@ -3,19 +3,19 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    size_t ip4;
+    size_t ip4;                         
 
     ip4 = 
-    ((stoul(argv[1]) << 24)
-    | (stoul(argv[2]) << 16)
-    | (stoul(argv[3]) << 8) 
-    | stoul(argv[4]))
-    & ((stoul(argv[5]) == 0)
+    ((stoul(argv[1]) << 24)             // bitshift first octet
+    | (stoul(argv[2]) << 16)            // bitshift second octet
+    | (stoul(argv[3]) << 8)             // bitshift third octet
+    | stoul(argv[4]))                   // fourth octet
+    & ((stoul(argv[5]) == 0)            // netmask
         ? 0
-        : (~0UL << (32 - stoul(argv[5]))));
+        : (~0UL << (32 - stoul(argv[5])))); // make mask from input
 
-    cout 
-        << ((ip4 >> 24) & 0xFF) << '.'
+    cout            // output the whole IP in correct order and format
+        << ((ip4 >> 24) & 0xFF) << '.'  
         << ((ip4 >> 16) & 0xFF) << '.'
         << ((ip4 >> 8) & 0xFF) << '.'
         << ((ip4) & 0xFF) << '\n';
