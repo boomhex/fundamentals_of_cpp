7486a5fd2cc4e032f55d6c35da919d0a15ad7ab2
boomhex <98780985+boomhex@users.noreply.github.com>
2025-09-07 10:05:31 +1000

    Finished ex7

---
+++ set1/7/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+										// Use namespace std so I don't have
+using namespace std;					// to type it and avoid long lines.
+
+int main(int argc, char* argv[])
+{
+	cout << "hello " 					// statement should print hello first
+         << (argc == 1 ? "world" 		// no arguments, so print world
+             : (argc == 2 ? argv[1] 	// 1 argument, should print first arg
+             : argv[stoul(argv[1])]))	// >1, should print the specified index
+         << "\n";						// newline char for neatness of output.
+}
