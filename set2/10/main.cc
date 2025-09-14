2303b86c4eaf79e68d7aa763963039e08102f647
RubenKorbeeck <rubenkorbeeck@gmail.com>
2025-09-14 16:12:50 +1000

    added coments

--- set2/10/main.cc
+++ set2/10/main.cc
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <string>
 
-using namespace std;                        // allows to omit std::
+using namespace std;                        // Allows to omit std::
 
 int main(int argc, char* argv[])
 {
-    string line;
-    getline(cin, line, '\n');
-    for (; isblank(line.front());){
-        line.erase(0, 1);
+    string line;                            // initialize string
+    getline(cin, line, '\n');               // get input
+    for (; isblank(line.front());)          // loop until no blank in front
+    {
+        line.erase(0, 1);                   // erase first char in string
     }
-    for (; isblank(line.back());){
-        line.pop_back();
+    for (; isblank(line.back());)           // loop until no blank in the back
+    {
+        line.pop_back();                    // remove last char in string
     }
-    cout << '`' << line << "'\n";
+    cout << '`' << line << "'\n";           // print output to output stream
 }
