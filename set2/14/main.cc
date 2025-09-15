ebaeb4a71fa3eb927a8aacad121d1dec60282eb6
RubenKorbeeck <rubenkorbeeck@gmail.com>
2025-09-15 20:37:25 +1000

    first attempt at uploading

--- set2/14/main.cc
+++ set2/14/main.cc
@@ -7,22 +7,31 @@ int main(int argc, char* argv[])
 {
     double input;
     cout << "Enter a value to compute the sqrt of (<= 0 to stop): ";
-    cin >> input;
-    if (input <= 0){
+    cin >> input;                                   // get input
+    if (input <= 0){                                // stop with non-positive
         return 0;
     }
-    double x0 = 1;
-    double x1 = x0 + (input - (x0*x0)) / (2*x0);;
+    double x0 = 1;                                  // start value x0
+    double x1 = x0 + (input - (x0*x0)) / (2*x0);    // calulate first x1
     
-    for (size_t i = 1; x0 != x1; i++)
+    for (size_t i = 1; x0 != x1; i++)               // loop until x0 == x1
     {
-        cout << i << ": x0: " << x0 << ", x1 = "<< x1 << ", sqr is: "<< x1*x1 << '\n';
+        cout << i                                   // print steps
+             << ": x0: " 
+             << x0 << ", x1 = "
+             << x1 << ", sqr is: "
+             << x1*x1 
+             << '\n';
         x0 = x1;
-        x1 = x0 + (input - (x0*x0)) / (2*x0);
+        x1 = x0 + (input - (x0*x0)) / (2*x0);       // calculate new x1
        
         
     }
     
-    cout << "sqrt("<< input << ") is " << x0 << '\n';
+    cout << "sqrt("                                 // print output
+         << input 
+         << ") is " 
+         << x0 
+         << '\n';
     
 }
--- set2/16/main.cc
+++ set2/16/main.cc
@@ -8,22 +8,25 @@ int main(int argc, char* argv[])
     for (string line; getline(cin, line);)  // loop through lines input stream
     {
         
-        size_t currentIndex = line.size();
-        size_t j = currentIndex - 1;
-        for (auto from = line.crbegin(), to = line.crend(); from != to; ++from)
+        size_t currentIndex = line.size();  // start index
+        size_t j = currentIndex - 1;        // size of character
+        // loop through all character from end to start
+        for (auto from = line.crbegin(), to = line.crend(); 
+            from != to; ++from)
         {
-            if ((*from & 0x80) == 0x00)
+            if ((*from & 0x80) == 0x00)     // single byte
             {
-                cout << *from;
+                cout << *from;              // print char
                 currentIndex = j;
                 j = j - 1;
             } 
-            else if ((*from & 0xC0) == 0x80)
-            {
-                j--;
+            else if ((*from & 0xC0) == 0x80)// multi-char code point
+            {  
+                j--;                         
             }
             else
             {
+                // print all bytes for current character
                 cout << line.substr(j, currentIndex - j);
                 currentIndex = j;
                 j--;
