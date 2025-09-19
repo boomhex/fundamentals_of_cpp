9d0451b1777fdc25876d0925622a275d2474420e
RubenKorbeeck <rubenkorbeeck@gmail.com>
2025-09-19 21:21:05 +1000

    shitty start of 19

---
+++ set3/19/ex1.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n)
+{
+    int result = 1;
+    for (int i = 2; i <= n; ++i)
+        result *= i;
+    return result;
+}
+
+int main()
+{
+    cout << "factorial(5) = " << factorial(5) << '\n';
+}
