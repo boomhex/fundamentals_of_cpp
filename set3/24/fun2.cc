42b78592041f140e30de3733e2fef13a8409a1de
RubenKorbeeck <rubenkorbeeck@gmail.com>
2025-09-24 00:08:23 +1200

    ex 24 and descr

---
+++ set3/24/fun2.cc
@@ -0,0 +1,8 @@
+#include "main.ih"
+
+void fun2(string const &str)
+{
+    size_t sum = 0;
+    for (size_t idx = 0; idx < str.length(); ++idx)
+        sum += str[idx];
+}
--- set3/24/main.cc
+++ set3/24/main.cc
@@ -1,8 +1,8 @@
-#include <iostream>
-
-using namespace std;
+#include "main.ih"
 
 int main(int argc, char *argv[])
 {
-
+    callValue(argv[0]);
+    callRef(argv[0]);
 }
+
