02e4bfba18a340d2e5b2dfd072913b2398a87fcc
boomhex <98780985+boomhex@users.noreply.github.com>
2025-09-24 09:17:14 +1200

    cleaned 18

--- set3/18/main.cc
+++ set3/18/main.cc
@@ -1,12 +1,9 @@
 #include "main.ih"
-#include <iostream>
 
 using namespace std;    // omit std::
 
 int main(int argc, char **argv)
 {
-    if (!structCall(argc, argv))    // Ending program if structCall fails
-        return 0;
-
-    boundCall(argc, argv);
+    if (structCall(argc, argv))
+        boundCall(argc, argv);
 }
