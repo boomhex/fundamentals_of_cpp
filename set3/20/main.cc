c0c79b283fc2815dc1a0c27c1dd74fa3ab6cdfa6
RubenKorbeeck <rubenkorbeeck@gmail.com>
2025-09-26 20:02:33 +1200

    changed 20

--- set3/20/main.cc
+++ set3/20/main.cc
@@ -6,28 +6,21 @@ using namespace std;                        // allows to omit std::
 int main(int const argc, char const *argv[])
 {
     // check if input is valid
-    if (argc != 2 ||
-        argv[1][0] != '-' ||
-        argv[1][1] == '\0' ||
-        argv[1][2] != '\0')
-        return 1;
+    char input = checkInput(argc, argv);
 
-    switch (argv[1][1])                 // check second character
+    switch (input)                 // check second character
     {
     case 'c':                           // count chars
         cout << countChars() << '\n';
     break;
-
     case 'w':                           // count words
         cout << countWords() << '\n';
     break;
-
     case 'l':                           // count lines
         cout << countLines() << '\n';
     break;
-    
     default:
-        return 0;
+    return 1;                           // input invalid
     }
 }
 
