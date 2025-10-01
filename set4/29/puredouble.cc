b16f93f9013cb11374b2ba40138a7629f96606ad
boomhex <98780985+boomhex@users.noreply.github.com>
2025-10-01 16:43:51 +1000

    Changed flow in puredouble.cc

--- set4/29/puredouble.cc
+++ set4/29/puredouble.cc
@@ -7,11 +7,7 @@ bool Parser::pureDouble(double *dest, string const &str)
     if (pos != str.size())  // Ensure whole string is parsed
         return false;
 
-    // Check if value is integral
-    if (str.find_first_of(".eE") == string::npos)
-        d_integral = true;  // No floating point chars found.
-    else
-        d_integral = false; // floating point chars found.
-
+    // Check whether value is integral
+    d_integral = (str.find_first_of(".eE") == string::npos);
     return true;
 }
