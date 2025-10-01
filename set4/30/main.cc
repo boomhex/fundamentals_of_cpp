6cddb6bef7a3cc072611a79e2e55ee03fab9e79d
RubenKorbeeck <rubenkorbeeck@gmail.com>
2025-10-02 09:52:46 +1000

    ex 31

--- set4/30/main.cc
+++ set4/30/main.cc
@@ -1,8 +1,6 @@
-// main.cc
 #include "calculator.h"
 
 int main() {
     Calculator calc;
-    calc.run();      // reads from std::cin, writes to std::cout
-    return 0;
+    calc.run();
 }
---
+++ set4/31/main.cc
@@ -0,0 +1,6 @@
+#include "calculator.h"
+
+int main() {
+    Calculator calc;        // create calculator
+    calc.run();             // run the calculator
+}
