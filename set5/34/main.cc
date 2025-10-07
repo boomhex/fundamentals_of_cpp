ee1d38960c3c315cf3ccd352748e50a12b033f5a
boomhex <98780985+boomhex@users.noreply.github.com>
2025-10-07 12:20:14 +0200

    fix ex. 34: removed debugging files 2

--- set5/34/main.cc
+++ set5/34/main.cc
@@ -3,11 +3,8 @@
 int main(int argc, char const **argv, char const **environ)
 {
     CopyCat firstCat;
-    firstCat.display();
 
     CopyCat secondCat{ environ };
-    secondCat.display();
 
     CopyCat thirdCat(argc, argv);
-    thirdCat.display();
 }
