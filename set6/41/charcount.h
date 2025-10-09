d9e3065b067ab1edac333321875a7df75ff31d4e
boomhex <98780985+boomhex@users.noreply.github.com>
2025-10-09 13:38:14 +0200

    Copied charcount into 41

---
+++ set6/41/charcount.h
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct Char
+{
+    unsigned char ch;
+    std::size_t freq;
+};
+
+struct CharInfo
+{
+    Char *ptr = nullptr;
+    std::size_t nCharObj = 0;
+};
+
+struct CharCount
+{
+    enum Action {APPEND, INSERT, INC};      // All actions
+    CharInfo d_info;
+    
+    public:
+        std::size_t count(std::istream &input);     // count.cc
+        CharInfo const &info() const;       // inline function
+    private:
+        Action locate(unsigned char ch, std::size_t &index) const;    // locat.cc
+};
+
+#include "info.f"
---
+++ set6/41/locate.cc
@@ -0,0 +1,14 @@
+#include "charcount.ih"
+
+CharCount::Action CharCount::locate(unsigned char ch, size_t &index) const
+{
+    for (index = 0; index != d_info.nCharObj; ++index)
+    {
+        if (d_info.ptr[index].ch == ch)     // if char is not new
+            return INC;
+
+        if (d_info.ptr[index].ch > ch)      // if char should be inserted
+            return INSERT;
+    }
+    return APPEND;                          // if char should be appended
+}
