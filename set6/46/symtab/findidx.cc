2b0fcd9e7716aa629e8897cb3d9666b4f481f78f
boomhex <98780985+boomhex@users.noreply.github.com>
2025-10-12 14:33:25 +0200

    changed bin search

--- set6/46/symtab/findidx.cc
+++ set6/46/symtab/findidx.cc
@@ -2,26 +2,26 @@
 
 size_t Symtab::findIdx(string const &ident)
 {
-    size_t left = 0;
-    size_t right = d_size;
-    size_t mid = (left + right) / 2;
+    size_t begin = 0;
+    size_t end = d_size;
+    size_t mid = (begin + end) / 2;
 
-    while (left < right)
+    while (begin != end)
     {
-        mid = (left + right) / 2;
+        mid = (begin + end) / 2;
 
         if (d_symbols[mid]->ident() < ident)      // Check lexicographic order
-            left = mid + 1;                           // And half search space
+            begin = mid + 1;                           // And half search space
         else
-            right = mid;
+            end = mid;
     }
 
     // Check if the name is found in existing array
-    if (left < d_size && d_symbols[left]->ident() == ident)
-        return left;
+    if (begin < d_size && d_symbols[begin]->ident() == ident)
+        return begin;
 
     // Create the new symbol if not found
     Symbol *newSymbol = new Symbol(ident, 0);
-    insert(left, newSymbol);
-    return left;
+    insert(begin, newSymbol);
+    return begin;
 }
