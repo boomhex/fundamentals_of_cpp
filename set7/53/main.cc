07b38643c2228d49d5f083a2ac87d2c2d6e9fc83
boomhex <98780985+boomhex@users.noreply.github.com>
2025-10-15 13:10:13 +0200

    Finished 53

--- set7/53/main.cc
+++ set7/53/main.cc
@@ -1,1 +1,30 @@
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double value = 12.04;
+
+    cout <<
+    "std display      " << '"' <<
+        setw(15) << value <<
+    '"' << '\n' <<
+    "left aligned     " << '"' <<
+        left << setw(15) << value
+    << '"' << '\n' <<
+    "right aligned    " << '"' <<
+        right << setw(15) << value
+    << '"' << '\n' <<
+    "3 digits         "
+    << '"' <<
+        setw(15) << setprecision(3) << showpoint << value
+    << '"' << '\n' <<
+    "4-digit fraction " << '"' <<
+        setw(15) << fixed << setprecision(4) << value
+    << '"' << '\n' <<
+    "std display.     " << '"' <<
+        setw(15) << defaultfloat << value
+    << '"' << '\n';   
+}
