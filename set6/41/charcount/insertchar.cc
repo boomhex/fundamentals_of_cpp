6ea7836602b36ecd5a04b5d5a0a1f7c777b04eda
boomhex <98780985+boomhex@users.noreply.github.com>
2025-10-16 23:20:45 +0200

    fix: CANON in insertchar.cc

--- set6/41/charcount/insertchar.cc
+++ set6/41/charcount/insertchar.cc
@@ -6,7 +6,7 @@ void CharCount::insertChar(size_t idx, unsigned char ch)
     ensureCapacity(d_info.nCharObj + 1);     // checks if new memory is needed
 
     // shift everything after index to the right 
-    for (size_t count = d_info.nCharObj; count > idx; --count)
+    for (size_t count = d_info.nCharObj; count != idx; --count)
         d_info.ptr[count] = d_info.ptr[count - 1];
 
     d_info.ptr[idx] = Char{ch, 1};          // insert char
